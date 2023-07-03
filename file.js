95ba4209e34693373519794bb46fb5ce3bc74a64
Santajit Patra <business.santajitpatra@gmail.com>
2023-07-04 01:35:20 +0530

    fs module
    how to use file system or fs

---
+++ file.js
@@ -0,0 +1,30 @@
+// const fs = require("fs");
+
+//Sync
+// fs.writeFileSync('output.txt', 'Hi there');
+
+//Async
+// fs.writeFile("output.txt", 'Hi there 2', (err, data) => {});
+
+// const result = fs.readFileSync("./contacts.txt", 'utf-8');
+// console.log(result)
+
+// fs.readFile("./contacts.txt", "utf-8", (err, data) => {
+//   if (err) {
+//     console.log("error", err);
+//   } else {
+//     console.log(data);
+//   }
+// });
+
+// fs.appendFileSync("./output.txt", new Date().getDate().toLocaleString());
+
+// fs.appendFileSync("./output.txt", `${Date.now()} Hey There 2\n`);
+
+// fs.cpSync("./output.txt", "./copy.txt");
+
+// fs.unlinkSync("./copy.txt")
+
+// console.log(fs.statSync("./output.txt").isFile());
+
+// fs.mkdirSync("my-docs/s/ddfd/my-docs")
