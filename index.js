f4975da5e9539330edcb9c5d6727fa2f9069d522
Santajit Patra <business.santajitpatra@gmail.com>
2023-07-03 23:23:21 +0530

    module learn
    How module works

---
+++ index.js
@@ -0,0 +1,3 @@
+const {add,sub} = require('./math');
+
+console.log("Math: ", sub(5,6));
---
+++ math.js
@@ -0,0 +1,11 @@
+function add(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+// exports.sub = (a+b) =>  a + b;
+
+module.exports = {add, sub};
